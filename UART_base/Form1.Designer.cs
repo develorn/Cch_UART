526e6052ba7fe08a927f99aa5064684d7a3e275d
Patrick <patrick.piega18@gmail.com>
2014-10-09 16:06:49 +0200

    Finished sample app USART

---
+++ UART_base/Form1.Designer.cs
@@ -0,0 +1,301 @@
+﻿namespace UART_base
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.txtPort = new System.Windows.Forms.ComboBox();
+            this.cmbStopbits = new System.Windows.Forms.ComboBox();
+            this.cmbDatabits = new System.Windows.Forms.ComboBox();
+            this.cmbParity = new System.Windows.Forms.ComboBox();
+            this.cmbBaudrate = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.cmdOpen = new System.Windows.Forms.Button();
+            this.cmdClose = new System.Windows.Forms.Button();
+            this.cmdSend = new System.Windows.Forms.Button();
+            this.txtDatatoSend = new System.Windows.Forms.TextBox();
+            this.txtReceive = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.flControl = new System.Windows.Forms.FlowLayoutPanel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtPort
+            // 
+            this.txtPort.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.txtPort.FormattingEnabled = true;
+            this.txtPort.Location = new System.Drawing.Point(35, 221);
+            this.txtPort.Name = "txtPort";
+            this.txtPort.Size = new System.Drawing.Size(61, 21);
+            this.txtPort.TabIndex = 0;
+
+            // 
+            // cmbStopbits
+            // 
+            this.cmbStopbits.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbStopbits.FormattingEnabled = true;
+            this.cmbStopbits.Items.AddRange(new object[] {
+            "1 ",
+            "2 "});
+            this.cmbStopbits.Location = new System.Drawing.Point(303, 221);
+            this.cmbStopbits.Name = "cmbStopbits";
+            this.cmbStopbits.Size = new System.Drawing.Size(61, 21);
+            this.cmbStopbits.TabIndex = 1;
+            // 
+            // cmbDatabits
+            // 
+            this.cmbDatabits.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbDatabits.FormattingEnabled = true;
+            this.cmbDatabits.Items.AddRange(new object[] {
+            "8",
+            "7 ",
+            "6 ",
+            "5 "});
+            this.cmbDatabits.Location = new System.Drawing.Point(236, 221);
+            this.cmbDatabits.Name = "cmbDatabits";
+            this.cmbDatabits.Size = new System.Drawing.Size(61, 21);
+            this.cmbDatabits.TabIndex = 2;
+            // 
+            // cmbParity
+            // 
+            this.cmbParity.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbParity.FormattingEnabled = true;
+            this.cmbParity.Items.AddRange(new object[] {
+            "None",
+            "Even",
+            "Odd",
+            "Mark",
+            "Space"});
+            this.cmbParity.Location = new System.Drawing.Point(169, 221);
+            this.cmbParity.Name = "cmbParity";
+            this.cmbParity.Size = new System.Drawing.Size(61, 21);
+            this.cmbParity.TabIndex = 3;
+            // 
+            // cmbBaudrate
+            // 
+            this.cmbBaudrate.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbBaudrate.FormattingEnabled = true;
+            this.cmbBaudrate.Items.AddRange(new object[] {
+            "1200",
+            "9600",
+            "19200",
+            "38400"});
+            this.cmbBaudrate.Location = new System.Drawing.Point(102, 221);
+            this.cmbBaudrate.Name = "cmbBaudrate";
+            this.cmbBaudrate.Size = new System.Drawing.Size(61, 21);
+            this.cmbBaudrate.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(51, 205);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Port:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(99, 205);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(61, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Baud Rate:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(181, 205);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(36, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Parity:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(245, 205);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(49, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Databits:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(300, 205);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(48, 13);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Stopbits:";
+            // 
+            // cmdOpen
+            // 
+            this.cmdOpen.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.cmdOpen.Location = new System.Drawing.Point(35, 312);
+            this.cmdOpen.Name = "cmdOpen";
+            this.cmdOpen.Size = new System.Drawing.Size(57, 22);
+            this.cmdOpen.TabIndex = 10;
+            this.cmdOpen.Text = "Open";
+            this.cmdOpen.UseVisualStyleBackColor = true;
+            this.cmdOpen.Click += new System.EventHandler(this.cmdOpen_Click);
+            // 
+            // cmdClose
+            // 
+            this.cmdClose.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.cmdClose.Location = new System.Drawing.Point(106, 312);
+            this.cmdClose.Name = "cmdClose";
+            this.cmdClose.Size = new System.Drawing.Size(57, 22);
+            this.cmdClose.TabIndex = 11;
+            this.cmdClose.Text = "Close";
+            this.cmdClose.UseVisualStyleBackColor = true;
+            this.cmdClose.Click += new System.EventHandler(this.cmdClose_Click);
+            // 
+            // cmdSend
+            // 
+            this.cmdSend.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.cmdSend.Location = new System.Drawing.Point(307, 312);
+            this.cmdSend.Name = "cmdSend";
+            this.cmdSend.Size = new System.Drawing.Size(57, 22);
+            this.cmdSend.TabIndex = 12;
+            this.cmdSend.Text = "Send";
+            this.cmdSend.UseVisualStyleBackColor = true;
+            this.cmdSend.Click += new System.EventHandler(this.cmdSend_Click);
+            // 
+            // txtDatatoSend
+            // 
+            this.txtDatatoSend.Location = new System.Drawing.Point(169, 314);
+            this.txtDatatoSend.Name = "txtDatatoSend";
+            this.txtDatatoSend.Size = new System.Drawing.Size(125, 20);
+            this.txtDatatoSend.TabIndex = 13;
+            // 
+            // txtReceive
+            // 
+            this.txtReceive.BackColor = System.Drawing.SystemColors.Control;
+            this.txtReceive.Location = new System.Drawing.Point(35, 45);
+            this.txtReceive.Multiline = true;
+            this.txtReceive.Name = "txtReceive";
+            this.txtReceive.ReadOnly = true;
+            this.txtReceive.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtReceive.Size = new System.Drawing.Size(328, 140);
+            this.txtReceive.TabIndex = 14;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Algerian", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(144, 13);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(111, 16);
+            this.label6.TabIndex = 15;
+            this.label6.Text = "Receive TXT";
+            // 
+            // flControl
+            // 
+            this.flControl.BackColor = System.Drawing.Color.DarkRed;
+            this.flControl.Location = new System.Drawing.Point(54, 290);
+            this.flControl.Name = "flControl";
+            this.flControl.Size = new System.Drawing.Size(16, 16);
+            this.flControl.TabIndex = 16;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button1.Location = new System.Drawing.Point(35, 13);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(57, 22);
+            this.button1.TabIndex = 17;
+            this.button1.Text = "Clear";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(395, 376);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.flControl);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txtReceive);
+            this.Controls.Add(this.txtDatatoSend);
+            this.Controls.Add(this.cmdSend);
+            this.Controls.Add(this.cmdClose);
+            this.Controls.Add(this.cmdOpen);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cmbBaudrate);
+            this.Controls.Add(this.cmbParity);
+            this.Controls.Add(this.cmbDatabits);
+            this.Controls.Add(this.cmbStopbits);
+            this.Controls.Add(this.txtPort);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(411, 414);
+            this.MinimumSize = new System.Drawing.Size(411, 414);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "USART";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox txtPort;
+        private System.Windows.Forms.ComboBox cmbStopbits;
+        private System.Windows.Forms.ComboBox cmbDatabits;
+        private System.Windows.Forms.ComboBox cmbParity;
+        private System.Windows.Forms.ComboBox cmbBaudrate;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button cmdOpen;
+        private System.Windows.Forms.Button cmdClose;
+        private System.Windows.Forms.Button cmdSend;
+        private System.Windows.Forms.TextBox txtDatatoSend;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.FlowLayoutPanel flControl;
+        private System.Windows.Forms.TextBox txtReceive;
+        private System.Windows.Forms.Button button1;
+
+
+    }
+}
+
---
+++ UART_base/Form1.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.IO.Ports;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace UART_base
+{
+    public partial class Form1 : Form
+    {
+        
+
+        public Form1()
+        {
+            InitializeComponent();
+            cmdClose.Enabled = false; //dezaktywacja klawisza zamykajacego polaczenie
+            cmdSend.Enabled = false;
+            cmbBaudrate.SelectedIndex = 1;   //domyslanie aby uzytkownik nigdy nie zapomnial
+            cmbParity.SelectedIndex = 0;
+            cmbDatabits.SelectedIndex = 0;
+            cmbStopbits.SelectedIndex = 0;
+            foreach (string s in System.IO.Ports.SerialPort.GetPortNames())  //przypisuje do s znalezione porty i dodaje do panelu. jest tak dlugo az bedzie puste getport
+            {
+                txtPort.Items.Add(s);
+            }  
+
+           
+        }
+
+        public System.IO.Ports.SerialPort sport;  //spot to klasa, krotsza nazwa poprostu
+        string InputData = String.Empty;
+        internal delegate void SetTextDelegate(string Text);
+
+        public void serialport_connect(string port, int baudRate, Parity parity, int dataBits, StopBits stopbits)
+        {
+            DateTime dt =  DateTime.Now; //pobranie aktualnej daty z struktury
+            string dtn = dt.ToShortTimeString();
+            sport = new System.IO.Ports.SerialPort(port,baudRate,parity,dataBits,stopbits); //nowy obiekt klasy o tych parametrach          
+
+            try
+            {
+                sport.Open(); //otwarcie portu
+                cmdOpen.Enabled = false; //zablokowanie przed ponownym otwarciem
+                cmdClose.Enabled = true; //odblokowanie aby zamknac polaczenie
+                cmdSend.Enabled = true;
+                txtReceive.AppendText("[" + dtn + "] " + " Connected\n");                                 
+                sport.DataReceived += new SerialDataReceivedEventHandler(sport_DataReceived); // handler do przerwania gdy nastapi nadejscie danych. datareceived tak samo jak w F12 napisane
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString(), "Error ;(");
+            }
+        }
+
+        private void sport_DataReceived(object sender, SerialDataReceivedEventArgs e) //gdy event zglosi ze cos przyszlo to przechodzi do tej metody
+        {
+            try
+            {
+                InputData = sport.ReadExisting();
+                IAsyncResult result = this.BeginInvoke(new SetTextDelegate(SetText), new object[] { InputData }); //wywolanie asynchroniczne (DELEGAT, ARGUMENT)
+                this.EndInvoke(result);
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("error");
+            }
+          
+        }
+
+        private void SetText(string text)
+        {
+            this.txtReceive.AppendText(text);            
+        }
+
+
+
+
+        private void cmdOpen_Click(object sender, EventArgs e)
+        {
+            try
+            {
+
+                int baudrate = Convert.ToInt32(cmbBaudrate.Text);
+                int databits = Convert.ToInt32(cmbDatabits.Text);
+                Parity parity = (Parity)Enum.Parse(typeof(Parity), cmbParity.Text);
+                string port = txtPort.Text;
+                StopBits stopbits = (StopBits)Enum.Parse(typeof(StopBits), cmbStopbits.Text);
+
+                serialport_connect(port, baudrate, parity, databits, stopbits);
+                flControl.BackColor = System.Drawing.Color.DarkGreen;
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Check your settings\n Port can't be open", "Error", MessageBoxButtons.OK, MessageBoxIcon.Warning, MessageBoxDefaultButton.Button1);                   
+                
+            }
+           
+
+        }
+
+        private void cmdSend_Click(object sender, EventArgs e)
+        {
+           try
+            {
+
+                DateTime dt = DateTime.Now;
+                string dtn = dt.ToShortTimeString();
+                string data = txtDatatoSend.Text;
+                InputData = txtDatatoSend.Text;
+                sport.Write(data);                
+                IAsyncResult result = this.BeginInvoke(new SetTextDelegate(SetText), data);         
+                this.EndInvoke(result);
+                this.txtReceive.AppendText("\n");
+ 
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Port is close\n Can't send data", "Error", MessageBoxButtons.OK, MessageBoxIcon.Warning, MessageBoxDefaultButton.Button1);                  
+            } 
+
+           
+        }
+
+        private void cmdClose_Click(object sender, EventArgs e)
+        {
+            DateTime dt = DateTime.Now;
+            string dtn = dt.ToShortTimeString();
+
+            if (sport.IsOpen)
+            {
+                sport.Close();
+                cmdClose.Enabled = false;
+                cmdOpen.Enabled = true;
+                cmdSend.Enabled = false;
+                txtReceive.AppendText("[" + dtn + "]" + " Disconnected\n");    
+                flControl.BackColor = System.Drawing.Color.DarkRed;  //zmiana koloru kontrolki
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            txtReceive.Clear();
+        }   
+       
+    }
+}
+
+
